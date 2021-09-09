b5def2474166624b2327eb028b289950685020ca
ricardobenzecry <ricardo.benzecry@gmail.com>
2021-09-09 15:40:53 -0300

    Adicionando nível de dificuldade

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let food ={
 }
 
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "black";    
     context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
@@ -32,12 +32,14 @@ function drawFood (){
 //quando um evento acontece, detecta e chama uma função
 document.addEventListener('keydown', update);
 
+
 function update(event){
     if(event.keyCode == 37 && direction != 'right') direction = 'left';
     if(event.keyCode == 38 && direction != 'down') direction = 'up';
     if(event.keyCode == 39 && direction != 'left') direction = 'right';
     if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
+
 
 function iniciarJogo(){    
 
@@ -50,13 +52,14 @@ function iniciarJogo(){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
             alert('Game Over :(');
+            
         }
     }
 
     criarBG();
     criarCobrinha();
     drawFood();
-
+     
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
@@ -66,10 +69,11 @@ function iniciarJogo(){
     if(direction == "down") snakeY += box;
 
     if(snakeX != food.x || snakeY != food.y){
-        snake.pop(); //pop tira o último elemento da lista
+        snake.pop(); //pop tira o último elemento da lista    
     }else{
         food.x = Math.floor(Math.random() * 15 +1) * box;
         food.y = Math.floor(Math.random() * 15 +1) * box;
+        
     }
     
     let newHead ={
@@ -80,4 +84,17 @@ function iniciarJogo(){
     snake.unshift(newHead); //método unshift adiciona como primeiro quadradinho da cobrinha
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+
+let  jogo = setInterval(iniciarJogo,400);
+
+function newLevel1(){     
+    setTimeout(function() {
+        setInterval(iniciarJogo,380);
+    }, 1000);      
+    alert("Novo nível");
+}
+ 
+
+ 
+
+
